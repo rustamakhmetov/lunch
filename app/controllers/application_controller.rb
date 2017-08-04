2f77ba6eca42b9f494050f828ae1ca3e8a395d91
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-08-04 12:52:40 +0500

    User edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation,
+                                                              :current_password, :name])
   end
 end

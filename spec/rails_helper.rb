6cb0935db0abfd5e80a593951d9bcf09a122f590
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2017-08-04 11:43:13 +0500

    Testing marked specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,9 @@ RSpec.configure do |config|
   config.extend ControllerMacros, type: :controller
   config.include MailerHelpers, type: :mailer
 
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
